859fbe536b0e1a8b7c548f104eb5567e54c75a95
Deepthir147 <deepthi.xworkz@gmail.com>
2023-07-20 23:37:14 +0530

    ConstructorExamplepark and Runner

---
+++ ParkRunner.java
@@ -0,0 +1,15 @@
+class ParkRunner{
+	public static void main(String[] args){
+		
+		Park mall = new Park("Rajkumar Park","Fashionpark","Bengaluru",2008);
+		
+		
+		
+		Park mall1 = new Park("park","Urban park","Mysore",2019);
+		
+		
+		
+		Park mall2=new Park("Park","Fashion park","Bengaluru",2008,"Jane",180,300,21);
+		
+	}
+}
