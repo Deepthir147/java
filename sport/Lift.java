aa14f41195a8577f78c3f201574e6b26421581ef
Deepthir147 <deepthi.xworkz@gmail.com>
2023-07-31 23:19:19 +0530

    association

---
+++ sport/Lift.java
@@ -0,0 +1,19 @@
+package com.xworkz.allprojects.sport;
+
+public class Lift {
+
+		
+		public int liftNo;
+		
+		public Lift(int liftNo) {
+			this.liftNo=liftNo;
+		}
+		
+		public void printInfo() {
+			System.out.println("Lift Numbe is: "+liftNo);
+		}
+
+	}
+
+
+
---
+++ sport/Player.java
@@ -0,0 +1,34 @@
+package com.xworkz.allprojects.sport;
+
+public class Player {
+	
+		
+		public String name;
+		public int exp;
+		public HomeTown town;
+		public Building building;
+		
+		public Player(String name,int exp, HomeTown town, Building building) {
+			
+			this.name=name;
+			this.exp=exp;
+			this.town=town;
+			this.building=building;
+			
+		}
+		
+		public void printInfo() {
+			
+			System.out.println("Player Name: "+name);
+			System.out.println("Player Experience: "+exp);
+			System.out.println("***********************************");
+			town.printInfo();
+			System.out.println("***********************************");
+			building.printInfo();
+			
+		}
+
+	}
+
+
+
